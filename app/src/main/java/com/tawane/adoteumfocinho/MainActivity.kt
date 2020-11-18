fe747ba8fbc8cdfa54231d1a1508e5de674db5ed
Tawane <tawanesilva.96@hotmail.com>
2020-11-18 17:10:27 -0300

    finalizando setando adapter com array

--- app/src/main/java/com/tawane/adoteumfocinho/MainActivity.kt
+++ app/src/main/java/com/tawane/adoteumfocinho/MainActivity.kt
@@ -2,10 +2,33 @@ package com.tawane.adoteumfocinho
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tawane.adoteumfocinho.adapter.ListaAdapter
 
 class MainActivity : AppCompatActivity() {
+    var listaAdapter: ListaAdapter? = null
+    var linearLayoutManager: LinearLayoutManager? = null
+    var nameList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.content_list)
+        setContentView(R.layout.activity_main)
+
+
+        val list: RecyclerView = findViewById(R.id.list_dogs)
+
+        getDogs()
+        listaAdapter = ListaAdapter(nameList, this)
+        linearLayoutManager = LinearLayoutManager(this)
+        list.layoutManager = linearLayoutManager
+        list.adapter = listaAdapter
+        list.adapter
+    }
+
+    private fun getDogs() {
+        nameList.add("Amora")
+        nameList.add("Junior")
+        nameList.add("Ane")
     }
 }
---
+++ app/src/main/java/com/tawane/adoteumfocinho/adapter/ListaAdapter.kt
@@ -0,0 +1,57 @@
+package com.tawane.adoteumfocinho.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.tawane.adoteumfocinho.R
+
+class ListaAdapter(nameList: List<String>, private var ctx: Context) :
+        RecyclerView.Adapter<ListaAdapter.ViewHolder>() {
+
+    private var nameList: List<String> = ArrayList<String>()
+
+    init {
+        this.nameList = nameList
+    }
+
+    // Aqui é onde o viewholder é criado a partir do layout
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(ctx).inflate(R.layout.content_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    // Nessa parte é onde se modifica o item do viewholder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val name = nameList[position]
+
+        holder.name.text = name
+
+        // Verifica se o nome for igual a Emy seta a cor e a imagem em si
+        if (name == "Amora") {
+            holder.name.setTextColor(ContextCompat.getColor(ctx, R.color.colorCledy))
+            holder.imagem.setBackgroundResource(R.drawable.img_caoum)
+        }
+
+        // Verifica se o nome for igual a Cledy seta a cor e a imagem em si
+        if (name == "Junior") {
+            holder.name.setTextColor(ContextCompat.getColor(ctx, R.color.colorCledy))
+            holder.imagem.setBackgroundResource(R.drawable.img_caodois)
+        }
+    }
+
+    // Devolve quantidade de itens do nameList
+    override fun getItemCount(): Int {
+        return nameList.size
+    }
+
+    // Aqui é a criação dos itens do viewholder
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var name = view.findViewById<TextView>(R.id.text_titulo)
+        var imagem = view.findViewById<ImageView>(R.id.img_cachorrinhoum)
+    }
+}
